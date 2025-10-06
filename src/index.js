9c0cc666f5e6463c19979379113c8f146f81081c
Mohammed-Ben-Cheikh <bencheikh.official@gmail.com>
2025-10-06 14:59:36 +0100

    feat: update .gitignore to include .env and improve response status in main route

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"));
+app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev",));
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
 app.get("/", (req, res) => {
-  res.json({ message: "holla from main root" });
+  res.status(200).json({ message: "holla from main root" });
 });
 
 app.listen(PORT, () => {
